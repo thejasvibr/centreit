b320a5ed888c64bf02b4c27672a2b6ed2a0412ca
thejasvi <thejasvib@gmai.com>
2020-05-13 13:16:09 +0200

    Implement reset button into game. Working version

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 '''Checks how many points the use can tile within the screen
 give a certain time. 
 
-In progress...
-> Managed to get reset function in place
-> Now need to get the reset button to trigger the reset 
-    > User's clicking on the button shouldn't cause a dot to appear!
-
 FEATURE LIST:
-    1. Add a reset button which bring the user to a fresh start
-    2. 
-
+    1. Make the CG dot color RED when it's out of the zone
+    2. Concentric ellipses/circles for each level - this will give the
+    user a sense of progression. Even at larger radii, the user can use a few
+    balancing points in a smaller radius to even out the CG.
 
 TODO:
     1. The safe zone's circle and the center used for calculations don't seem 
@@ -18,9 +14,12 @@ TODO:
 
 '''
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import StringProperty, ReferenceListProperty, NumericProperty
 from kivy.graphics import Ellipse, Color, InstructionGroup
+from math import sqrt
 import random
 import copy
 
@@ -30,6 +29,9 @@ class TargetCircle(Widget):
     circle_center=[0,0]
 
 class CenterDot(Widget):
+    pass
+
+class Reset(Widget):
     pass
 
 class TileGame(Widget):
@@ -42,7 +44,9 @@ class TileGame(Widget):
     cg_y = NumericProperty(0)
     centre_gravity = ReferenceListProperty(cg_x,cg_y)
     touch_number = 0
-    cg_dot_size = 20
+    cg_dot_size = 10
+    cg_dot_r = sqrt(2*20**2)
+    
     
     cg_xy = []
     
@@ -111,7 +115,7 @@ class TileGame(Widget):
             else:
                 self.status = 'Saaaafe'
                 
-    def reset_action(self, button):
+    def reset_action(self, instance):
         '''
         Resets important aspects of the game
         
@@ -128,15 +132,29 @@ class TileGame(Widget):
         self.status = 'Saaafe'
         self.lives_left = 5
         self.all_points = []
-        self.touch_number = 0
+        self.num_points = '0'
+        self.centre_gravity = [0,0]
         
         for each_dot in self.all_dots:
             self.canvas.remove(each_dot)
 
 
+class MainWindow(BoxLayout):
+    def __init__(self, **kwargs):
+        super(MainWindow, self).__init__(**kwargs)
+        self.cols = 2
+        self.game_region = TileGame()
+        self.btn1 = BoxLayout()
+        self.btn1.size_hint = (0.2,1)
+        clear_button = Button(text='Reset', on_press=self.game_region.reset_action)
+        self.btn1.add_widget(clear_button)
+        self.add_widget(self.btn1)
+        self.add_widget(self.game_region)
+
+
 class TileItApp(App):
     def build(self): 
-        return TileGame()
+        return MainWindow()
 
 if __name__ == '__main__':
 	TileItApp().run()
